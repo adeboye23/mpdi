bd47cf3b76a00773f2675b2371c3dfec8a5c8b1f
Adeboye Oluwatobi <tobiadeboye23@gmail.com>
2026-03-09 00:40:24 +0100

    Remove unused db-postgres file

--- lib/db-postgres.ts
+++
@@ -1,8 +0,0 @@
-import { sql } from '@vercel/postgres'
-
-export async function query(text: string, params?: any[]) {
-  const result = await sql.query(text, params)
-  return result
-}
-
-export { sql }
